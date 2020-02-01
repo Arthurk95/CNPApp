832a2f8a73aabc75adc29bd457c5a34bfd61f73b
raymondn <raymondn@ecs.csus.edu>
2020-02-01 00:25:12 -0800

    added ugly print statements client side from db

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,13 +4,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express'});
+});
+
+router.get('/myvar', function(req, res){
   var sql = "SELECT * from customers;";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log(result);
+    res.send(JSON.stringify(result));
   });
-  
-  res.render('index', { title: 'Express' });
-});
-
+ });
 module.exports = router;
