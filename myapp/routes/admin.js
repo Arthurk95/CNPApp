abe1fd22b53b4a84edd7794a0082f7fec15e714a
raymondn <raymondn@ecs.csus.edu>
2020-02-05 18:27:16 -0800

    added nodemon and updated package.js so npm start will now auto start. also added blank pages to index buttons

---
+++ myapp/routes/admin.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express'});
+});
+
+router.get('/myvar', function(req, res){
+  var sql = "SELECT * from customers;";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+ });
+module.exports = router;
