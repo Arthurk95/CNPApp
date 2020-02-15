a7909799b1c97e78959b3e25e3ed5d543236d747
Arthur Kharit <arthurkharit@csus.edu>
2020-02-15 15:02:22 -0800

    add activities list to route for students page

--- myapp/routes/students.js
+++ myapp/routes/students.js
@@ -4,15 +4,19 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var student_query = "SELECT * FROM Students ORDER BY StudentName"; 
+  var activity_query = "SELECT * FROM Activities ORDER BY ActivityName;";
   /* Students of current day */
   /* var student_query = "CALL PullUnhiddenStudents();"; */
 
   /* All students [for testing purposes right now] */
   /* var student_query = "CALL ShowAllStudents();"; */
 
-  con.query(student_query, function (err, result) {
+  con.query(student_query, function (err, sQuery) {
     if (err) throw err;
-    res.render('students.ejs', { title: 'Student Page', students: result });
+    con.query(activity_query, function (err, aQuery) {
+      if (err) throw err;
+      res.render('students.ejs', {title: 'Student Page', students: sQuery,  activities: aQuery});
+    })
   })
 });
 
