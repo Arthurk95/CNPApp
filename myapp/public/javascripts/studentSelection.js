9c8ee6d2c6d07b04fe539fee4d219e0eb8fcc6ed
Arthur Kharit <arthurkharit@csus.edu>
2020-02-15 15:00:34 -0800

    added code to support selecting either students or activities, with many supporting functions

--- myapp/public/javascripts/studentSelection.js
+++ myapp/public/javascripts/studentSelection.js
@@ -1,27 +1,137 @@
 var selectedStudentIDs = [];
 var numSelectedStudents = 0;
-var footer = document.getElementById("footer");
+var selectedActivityID = undefined;
 
-function selectedStudent(element, studentID){
+var footer;
+var selectButton;
+var submitButton;
+var toSubmit = false;
+$(document).ready(function(){
+    footer = document.getElementById("footer");
+    selectButton = document.getElementById("selectButton");
+    submitButton = document.getElementById("submitButton");
+})
+
+
+function selectedStudent(studentElement, studentID){
+    // the passed student is being de-selected
+    if(studentElement.classList.contains("selectedStudent")){
+        studentElement.classList.remove("selectedStudent");
+        removeStudentFromList(studentID);
+        if(numSelectedStudents == 0){
+            hideElement(selectButton);
+        }
+    }
+    // the passed student is being selected
+    else {
+        studentElement.classList += " selectedStudent";
+        addStudentToList(studentID);
+        // first selected student -> show "Select Activity" button
+        if(numSelectedStudents == 1){
+            showButton("Activity");
+        }
+    }
+}
+
+function removeStudentFromList(studentID){
+    numSelectedStudents--;
+    var indexOf = selectedStudentIDs.indexOf(studentID);
+    selectedStudentIDs.splice(indexOf, 1);
+}
+
+function addStudentToList(studentID){
+    numSelectedStudents++;
+    selectedStudentIDs.push(studentID);
+}
+
+function showButton(buttonText){
+    if(toSubmit){
+        showElement(submitButton, "inline-block");
+    }
+    else{
+        showElement(selectButton, "inline-block");
+        selectButton.text = "Select " + buttonText;
+    }
+}
+
+/* Activity <li> element clicked, either make it green or de-select it */
+function selectedActivity(element, activityId){
     if(element.classList.contains("selectedStudent")){
         element.classList.remove("selectedStudent");
-        numSelectedStudents--;
-        var indexOf = selectedStudentIDs.indexOf(studentID);
-        selectedStudentIDs.splice(indexOf, 1);
-        if(numSelectedStudents == 0){
-          $(function(){
-              $("footer").children().fadeOut("fast");
-          })  
+        selectedActivityID = undefined;
+        hideElement(submitButton);
+    }
+    else{
+        if(selectedActivityID != undefined){
+            clearActivitySelection();
+        }
+        
+        selectedActivityID = activityId;
+        element.classList += "selectedStudent";
+        showButton("Students");
+    }
+}
+
+// Removes any selected Activities (green) since we can only do one at a time
+function clearActivitySelection(){
+    var activitiesList = document.getElementById("activities");
+    activitiesList = activitiesList.getElementsByTagName("li");
+    for (var i = 0; i < activitiesList.length; i++){
+        if(activitiesList[i].classList.contains("selectedStudent")){
+            var oldSelection = activitiesList[i];
+            oldSelection.classList.remove("selectedStudent");
+            break;
         }
     }
-    else {
-        element.classList += " selectedStudent";
-        numSelectedStudents++;
-        selectedStudentIDs.push(studentID);
-        if(numSelectedStudents == 1){
-          $(function(){
-              $("footer").children().fadeIn("fast");
-          })  
-        }
+    oldSelection.classList.remove("selectedStudent");
+}
+
+function hideActivities(){document.getElementById("activities").style.display = "none";}
+function showActivities(){document.getElementById("activities").style.display = "block";}
+
+function hideStudents(){document.getElementById("students").style.display = "none";}
+function showStudents(){document.getElementById("students").style.display = "block";}
+
+// The "Students" button was pushed when first coming to the page
+function startWithStudents(){
+    hideButtons();
+    showStudents();
+}
+
+function hideButtons(){
+    document.getElementById("studentButton").style.display = "none";
+    document.getElementById("activitiesButton").style.display = "none";
+}
+
+// The "Activities" button was pushed when first coming to the page
+function startWithActivities(){
+    hideButtons();
+    showActivities();
+}
+
+// activity or student(s) was chosen
+function selectionMade(){
+    hideElement(selectButton);
+    // students were selected first
+    if(selectedActivityID == undefined){
+        hideStudents();
+        showActivities();
     }
+    else{
+        hideActivities();
+        showStudents();
+    }
+    toSubmit = true;
+}
+
+// activity and student(s) chosen, submit to database
+function submitToDB(){
+    // do something database-y with selectedStudentIDs and selectedActivityID
+}
+
+function hideElement(element) { 
+    element.style.display = "none"; 
+}
+function showElement(element, displayType) { 
+    element.style.display = displayType; 
 }
