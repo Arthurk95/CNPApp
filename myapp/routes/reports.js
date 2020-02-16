c9c607744d366d36bf3d0820310d2ba935156d71
skiloh <shannonfaith1@gmail.com>
2020-02-16 11:01:14 -0800

    chnaged reports.js

--- myapp/routes/reports.js
+++ myapp/routes/reports.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var entry_query = "SELECT * FROM DailyActivities ORDER BY StudentId;";
+    var entry_query = "SELECT * FROM DailyActivities;";
     con.query(_query, function (err, result) {
       if (err) throw err;
       res.render('reports.ejs', { title: 'CNP Daily Report', reports: result });
