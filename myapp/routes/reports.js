8fc56df4fb1f5919ae03ca61110a7e9d782ec877
Arthur Kharit <arthurkharit@csus.edu>
2020-02-17 13:16:42 -0800

    changed variable names

--- myapp/routes/reports.js
+++ myapp/routes/reports.js
@@ -14,41 +14,40 @@ router.get('/', function(req, res, next) {
     listOfActivities: []
   };
 
-  var daily_query = "CALL PullUnhiddenStudents();";
-  con.query(daily_query, function (err, dailyStudents) {
-    if (err) throw err;
-    console.log(dailyStudents);
-    for(var i = 0; i < (dailyStudents[0].length); i++){
-      Student.id = dailyStudents[0][i].StudentId;
-      Student.name = dailyStudents[0][i].StudentName;
-      Students.push({
-        id: Student.id,
-        name: Student.name,
-        listOfActivities: Student.listOfActivities
-      });
+    var daily_query = "CALL PullUnhiddenStudents();";
+    con.query(daily_query, function (err, dailyStudents) {
+      if (err) throw err;
+      
+      for(var i = 0; i < (dailyStudents[0].length); i++){
+        Student.id = dailyStudents[0][i].StudentId;
+        Student.name = dailyStudents[0][i].StudentName;
+        Students.push({
+          id: Student.id,
+          name: Student.name,
+          listOfActivities: Student.listOfActivities
+        });
+        numStudents++;
+      }
       var today = '"2020-02-16"';
-      numStudents++;
-    }
 
-    for(var i = 0; i < Students.length; i++){
-      activities_query = "CALL ShowStudentDailyActivities(" + Students[i].id + 
-        ", " + today + ");";
+      for(var i = 0; i < Students.length; i++){
+        activities_query = "CALL ShowStudentDailyActivities(" + Students[i].id + 
+            ", " + today + ");";
 
-      con.query(activities_query, function(err, activities){
-        if(err) throw err;
-        for(var j = 0; j < numStudents; j++){
-          for(var k = 0; k < activities[0].length; k++){
-            Students[j].listOfActivities.push(activities[0][k].ActivityName);
-          }
-        }
-        //console.log(Students);
-        res.render('reports', { title: 'CNP Daily Report', report: Students });
+        con.query(activities_query, function(err, act){
+          if(err) throw err;
+          activities = act[0];
+        })
+        console.log(activities);
+        Students[i].listOfActivities.push(activities[0]);
+        
+        activities = [];
 
-      }) 
-    }
-    
-    
-  })
-});
+      }
+      res.render('reports.ejs', { title: 'CNP Daily Report', reports: Students });
+     
+      
+    })
+  });
 
   module.exports = router;
