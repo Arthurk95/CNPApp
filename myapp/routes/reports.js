ea92570f022c4c208adce808ae9c4d8512f8c1cc
raymondn <raymondn@ecs.csus.edu>
2020-02-17 15:45:13 -0800

    de-asynced reports

--- myapp/routes/reports.js
+++ myapp/routes/reports.js
@@ -1,53 +1,47 @@
 var express = require('express');
 var router = express.Router();
-var activities = [];
-
-var Students = [];
-var numStudents = 0;
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  var Student = {
-    id: 0,
-    name: "",
-    listOfActivities: []
-  };
+  var Students = [];
+  var daily_query = "CALL PullUnhiddenStudents();";
+  con.query(daily_query, function (err, dailyStudents) {
+    if (err) throw err;
+    recurseDailies(Students,dailyStudents,0,res);
+  })
 
-    var daily_query = "CALL PullUnhiddenStudents();";
-    con.query(daily_query, function (err, dailyStudents) {
-      if (err) throw err;
-      
-      for(var i = 0; i < (dailyStudents[0].length); i++){
-        Student.id = dailyStudents[0][i].StudentId;
-        Student.name = dailyStudents[0][i].StudentName;
-        Students.push({
-          id: Student.id,
-          name: Student.name,
-          listOfActivities: Student.listOfActivities
+  function recurseDailies(Students,dailyStudents,i,res){
+    var Student = { id: 0, name: "", listOfActivities: []};
+    if(i < (dailyStudents[0].length)){
+      Student.id = dailyStudents[0][i].StudentId;
+      Student.name = dailyStudents[0][i].StudentName;
+    
+      Students.push({
+        id: Student.id,
+        name: Student.name,
+        listOfActivities: []
+      });
+      activities_query = "CALL ShowStudentDailyActivitiesToday(" + Students[i].id + ");";
+      con.query(activities_query, function(err, act){
+        if(err) throw err;
+        var looper = act[0];
+        looper.forEach(element => {
+          if(element){
+            Students[i].listOfActivities.push(element);
+          }
         });
-        numStudents++;
-      }
-      var today = '"2020-02-16"';
-
-      for(var i = 0; i < Students.length; i++){
-        activities_query = "CALL ShowStudentDailyActivities(" + Students[i].id + 
-            ", " + today + ");";
-
-        con.query(activities_query, function(err, act){
-          if(err) throw err;
-          activities = act[0];
-        })
-        console.log(activities);
-        Students[i].listOfActivities.push(activities[0]);
-        
-        activities = [];
-
-      }
-      res.render('reports.ejs', { title: 'CNP Daily Report', reports: Students });
-     
-      
-    })
-  });
-
+        recurseDailies(Students,dailyStudents,i+1,res);
+        if(i == (dailyStudents[0].length) - 1){
+          bottomLayer(res, Students);
+        }
+      });
+    }
+    if(0==(dailyStudents[0].length)){
+      bottomLayer(res, Students);
+    }
+    
+  }
+});
+function bottomLayer(res,Students){
+  res.render('reports.ejs', { title: 'CNP Daily Report', reports: Students });
+}
   module.exports = router;
