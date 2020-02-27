ed2497f37c762eb8058032cf2f3afbc4befb0702
Matthew Kint <matt.kint@gmail.com>
2020-02-26 17:28:19 -0800

    session dependency

--- myapp/app.js
+++ myapp/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require("body-parser");
 var logger = require('morgan');
 var sql = require('mysql');
@@ -31,6 +32,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(session({
+  secret: "temp dev secret",
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
