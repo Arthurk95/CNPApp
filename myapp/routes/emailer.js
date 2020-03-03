586697e8a99b05285c51e412b57d00dcf81575ef
arthurk95 <arthurkharit@csus.edu>
2020-03-02 21:51:06 -0800

    added students and activities to emailer router

--- myapp/routes/emailer.js
+++ myapp/routes/emailer.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('emailer.ejs', { title: 'Express'});
+  var activity_query = "CALL PullUnhiddenActivities";
+  /* Students of current day */
+  var student_query = "CALL PullUnhiddenStudents();";
+
+  /* All students [for testing purposes right now] */
+  /* var student_query = "CALL ShowAllStudents();"; */
+
+  con.query(student_query, function (err, sQuery) {
+    if (err) throw err;
+    console.log(sQuery);
+        con.query(activity_query, function (err, aQuery) {
+        if (err) throw err;
+        res.render('emailer.ejs', {title: 'Student Page', students: sQuery[0],  activities: aQuery[0]});
+        })
+    });
   });
 
 
