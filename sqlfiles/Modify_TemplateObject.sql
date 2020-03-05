2cdea3c92763fcf9dde4f7cdc8dd2ebb65e92dd1
Joseph Nehl <jo3n3hl@gmail.com>
2020-03-04 18:11:29 -0800

    fixed add and delete template objects

--- sqlfiles/Modify_TemplateObject.sql
+++ sqlfiles/Modify_TemplateObject.sql
@@ -17,6 +17,15 @@ END $$
 DELIMITER $$
 CREATE PROCEDURE `cnp_data`.`DeleteTemplateObject` (IN id INT)
 BEGIN
+	SELECT NameOf FROM TemplateObject WHERE TemplateId=id INTO @nam;
+	SET @nam1 = CONCAT(@nam,"Note");
+	SET @sqlstm = CONCAT(
+			"ALTER TABLE `Behavior`
+				DROP COLUMN `", @nam , "`,
+				DROP COLUMN `", @nam1, "`");
+	PREPARE stmt FROM @sqlstm;
+	EXECUTE stmt;
+	DEALLOCATE PREPARE stmt;
 	DELETE FROM `TemplateObject` WHERE `TemplateId`=id;
 END $$
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -28,8 +37,17 @@ CREATE PROCEDURE `cnp_data`.`AddTemplateObject` (IN nam VARCHAR(45),
                                                  IN c4 VARCHAR(45),
                                                  IN c5 VARCHAR(45))
 BEGIN
-INSERT INTO TemplateObject (NameOf, CategoryOne, CategoryTwo, CategoryThree, CategoryFour, CategoryFive)
-	VALUES(nam, c1, c2, c3, c4, c5);
+	INSERT INTO TemplateObject (NameOf, CategoryOne, CategoryTwo, CategoryThree, CategoryFour, CategoryFive)
+		VALUES(nam, c1, c2, c3, c4, c5);
+	SET @nam = nam;
+	SET @nam1 = CONCAT(@nam,"Note");
+	SET @sqlstm = CONCAT(
+			"ALTER TABLE `Behavior`
+				ADD COLUMN `", @nam , "` VARCHAR(45),
+				ADD COLUMN `", @nam1, "` VARCHAR(500);");
+	PREPARE stmt FROM @sqlstm;
+	EXECUTE stmt;
+	DEALLOCATE PREPARE stmt;
 END $$
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 DELIMITER $$
