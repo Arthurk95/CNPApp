08ad4d2ceedb575fe2c94490e52a990f279524f4
Matthew Kint <matt.kint@gmail.com>
2020-03-19 11:26:18 -0700

    added port listener

--- myapp/app.js
+++ myapp/app.js
@@ -60,4 +60,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, function () {
+  console.log("Express server listening on port 3000");
+});
+
 module.exports = app;
