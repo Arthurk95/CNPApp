c5a058443b1606635349e5398906c296a7d00687
Matthew Kint <matt.kint@gmail.com>
2020-03-30 14:07:25 -0700

    user login dependencies

--- myapp/app.js
+++ myapp/app.js
@@ -1,8 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('express-flash');
+const methodOverride = require('method-override');
 var bodyParser = require("body-parser");
 var logger = require('morgan');
 var sql = require('mysql');
@@ -10,7 +17,13 @@ var fs = require('fs'), configPath = './config.json';//credentials file
 var parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 con = sql.createPool({connectionLimit : 100,"host":parsed.host,"user":parsed.user,"password":parsed.password,"database":parsed.database});
 weatherdata = "http://api.openweathermap.org/data/2.5/weather?id=" + parsed.sacramentoid + "&appid=" + parsed.weatherkey;
+const initializePassport = require('./passport-config');
+// const auth = require('./public/javascripts/loginScripts')
+initializePassport(passport, con);
 var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
+var logoutRouter = require('./routes/logout');
 var trackerRouter = require('./routes/tracker');
 var adminRouter = require('./routes/admin');
 var reportsRouter = require('./routes/reports');
@@ -29,14 +42,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(session({
-  secret: "temp dev secret",
-  resave: true,
-  saveUninitialized: true
-}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+// app.use(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use('/logout', logoutRouter);
 app.use('/tracker', trackerRouter);
 app.use('/admin', adminRouter);
 app.use('/reports', reportsRouter);
