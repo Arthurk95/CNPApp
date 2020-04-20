989723eddf7e2ecb1e4964d54cea33c4e56587ff
arthurk95 <arthurkharit@csus.edu>
2020-04-19 17:41:35 -0700

    added search function to activities (which can be re-used for other stuff)

--- myapp/public/javascripts/utilityScripts.js
+++ myapp/public/javascripts/utilityScripts.js
@@ -59,3 +59,35 @@ function openForm(formID){
     formElement = document.getElementById(formID);
     formElement.style.display = "block";
 }
+
+/* SEARCH FUNCTIONALITY FUNCTIONS */
+function toggleSearch(inputID, titleID){
+    var inputElement = document.getElementById(inputID);
+    var titleElement = document.getElementById(titleID);
+    if(inputElement.style.display === "none"){
+        inputElement.style.display = "block";
+        titleElement.style.display = "none";
+    }
+    else{
+        inputElement.style.display = "none";
+        titleElement.style.display = "block";
+    }
+}
+
+function updateSearch(elementToSearchID, input){
+    var filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    var li = document.getElementById(elementToSearchID).getElementsByTagName('li');
+
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        var p = li[i].getElementsByTagName("p")[0];
+        var txtValue = p.textContent || p.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
