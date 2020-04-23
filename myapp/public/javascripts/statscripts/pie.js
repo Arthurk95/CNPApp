dd48170c6feadbe53d04016752cdcb7c227667e0
Hannah <hannahdolan4@gmail.com>
2020-04-23 14:41:09 -0700

    pie chart updates

--- myapp/public/javascripts/statscripts/pie.js
+++ myapp/public/javascripts/statscripts/pie.js
@@ -102,9 +102,10 @@ class thePie {
         div.transition()
              .duration(50)
              .style("opacity", 1);
-        div.html("Total: " + d.value)
-             .style("left", (d3.event.pageX + 10) + "px")
-             .style("top", (d3.event.pageY - 15) + "px");
+        let percent = (Math.round((d.value / d.value) * 100)).toString() + '%';
+        div.html("Total: " + d.value + "\n Percent: %") //+ percent)
+             .style("left", (d3.event.pageX + 20) + "px")
+             .style("top", (d3.event.pageY + 20) + "px");
       })
 
       .on("mouseout", function(d){
