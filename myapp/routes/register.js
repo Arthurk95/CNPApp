9d740b2db60c66ef6d8425de649597c9b6dab9ff
Matthew Kint <matt.kint@gmail.com>
2020-05-07 16:30:04 -0700

    login, register, reset-password

--- myapp/routes/register.js
+++ myapp/routes/register.js
@@ -3,28 +3,31 @@ var router = express.Router();
 const auth = require('../public/javascripts/loginScripts');
 const bcrypt = require('bcrypt');
 
-router.get('/', auth.checkNotAuthenticated, (req, res) => {
+router.get('/', auth.checkAuthenticated, (req, res) => {
     res.render('register.ejs');
 })
 
 //need async for try/catch ?
-router.post('/', auth.checkNotAuthenticated, async (req, res) => {
+router.post('/', auth.checkAuthenticated, async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10); //10 is good hash default value
         push_user = `INSERT INTO Admins (Username, Email, Passwords, Names) VALUES ("${req.body.name}", "${req.body.email}", "${hashedPassword}", "temp");`
         con.query(push_user, function (err, result) {
             if (err) {
                 console.log(err);
-                throw err;
+                res.send(err.sqlMessage)
+            } else {
+                res.redirect('/login');
             }
         });
         //if successful then:
-        res.redirect('/login');
+        // res.redirect('/login');
     } catch (e) {
         //if unsuccessful
         console.log(e);
         res.redirect('/register');
     };
 });
+
 
 module.exports = router;
