7ed9ce53aca8fcb88f902f8417ef21619e833364
arthurk95 <arthurkharit@csus.edu>
2020-05-09 21:20:32 -0700

    save and reset buttons now work - need to refresh page on rest

--- myapp/routes/styleSettings.js
+++ myapp/routes/styleSettings.js
@@ -9,28 +9,63 @@ var themeColors = [];
 var textColors = [];
 var contentColors = [];
 var otherVariables = [];
-var originalFile;
+var originalFileContent;
 var root;
+var newRoot;
+var originalRoot;
 var hasBeenInitialized = false;
 
 /* Reads style.css file and puts its values into an array of objects, each object containing a Name and Value field */
 router.get('/', auth.checkAuthenticated, (req, res) => {
-    if(hasBeenInitialized){res.render("styleSettings.ejs", {themeColors: themeColors, textColors: textColors, contentColors: contentColors, otherVariables: otherVariables});}
-    else{
-        fs.readFile('./public/stylesheets/style.css', function(err, data) {
-            originalFile = data.toString("utf8");
-            getRoot(originalFile);
-            hasBeenInitialized = true;
-            res.render("styleSettings.ejs", {themeColors: themeColors, textColors: textColors, contentColors: contentColors, otherVariables: otherVariables});
-        });
+    clearVariables();
+    fs.readFile('./public/stylesheets/style.css', function(err, data) {
+        originalFileContent = data.toString("utf8");
+        getRoot(originalFileContent);
+        hasBeenInitialized = true;
+        res.render("styleSettings.ejs", {themeColors: themeColors, textColors: textColors, contentColors: contentColors, otherVariables: otherVariables});
+    });
+});
+
+router.post('/write', auth.checkAuthenticated, function (req, res, next) {
+    var lines = root.split('\n');
+    for(var i = 0; i < req.body.varName.length; i++){
+        for(var j = 0; j < lines.length; j++){
+            var line = lines[j].trim();
+            if(line.charAt(0) === '}'){j = lines.length;}
+            else if(line.includes("--" + req.body.varName[i] + ":")){
+                if(line.includes("-Original")){}
+                if(!line.includes(req.body.varVal[i])){ // variable value is different than already written value
+                    var newLine = "  --" + req.body.varName[i] + ": " + req.body.varVal[i] + ";";
+                    replaceLineWith(newLine, j);
+                    j = lines.length;
+                }
+            }
+        }
     }
-    
-  // res.render('index', { email: req.user.email });
+
+    if(root === originalRoot){}// nothing changed
+    else{
+        var newContent = originalFileContent.replace(originalRoot, root);
+        fs.writeFile("./public/stylesheets/style.css", newContent, function (err) {
+            if (err) throw err;
+          });
+    }
+    res.end();
+});
+
+router.get('/reset-to-defaults', auth.checkAuthenticated, (req, res) => {
+    fs.readFile('./public/stylesheets/style-backup.css', function(err, data) {
+        fs.writeFile("./public/stylesheets/style.css", data.toString("utf8"), function (err) {
+            if (err) throw err;
+            res.end();
+        });
+    });
 });
 
 function getRoot(data){
     var startingIndex = data.indexOf(":root");
     root = data.substr(startingIndex, data.indexOf('}'));
+    originalRoot = root;
     getVariables(root);
 }
 
@@ -43,7 +78,7 @@ function getVariables(root){
         if(line.substr(0,2) === "--"){
             var vals = line.split(':');
             var name = vals[0].split("--")[1]; // split removes dashes
-            var value = vals[1].substr(0, vals[1].length-2); // substr removes semi-colon
+            var value = vals[1].substr(0, vals[1].length-1).trim(); // substr removes semi-colon
 
             if(name.includes("-Original")){} // don't display original values so we can restore defaults
             else{
@@ -66,14 +101,20 @@ function getVariables(root){
     }
 }
 
-function nextOccurance(character, startingIndex){
-    var index;
-    for(var i = startingIndex; i < root.length; i++){
-        if(root.charAt(i) === character){
-            index = i;
-            i = root.length + 1;
-        }
-    }
-    return index;
+// made this because substr and trim() affected other variables as well (pointers or something idk)
+function replaceLineWith(line, lineIndex){
+    var lines = originalRoot.split('\n');
+    console.log(lineIndex + ": " + lines[lineIndex]);
+    console.log(lineIndex + ": " + line);
+    lines[lineIndex] = line;
+    root = lines.join('\n');
 }
+
+function clearVariables(){
+    themeColors = [];
+    textColors = [];
+    contentColors = [];
+    otherVariables = [];
+}
+
 module.exports = router;
