6e6fb26f6278c1e62c5e4a935e2f164fb26321ec
Matthew Kint <matt.kint@gmail.com>
2020-06-10 10:58:14 -0700

    remove cc for testing

--- myapp/routes/emailer.js
+++ myapp/routes/emailer.js
@@ -344,7 +344,7 @@ router.post('/send', (req, res) => {
         from: '"Creative Nature Playschool" <cnp.daily.report@gmail.com>', // sender address
         to: parent_emails, // list of receivers
         subject: "CNP Daily Report", // Subject line
-        cc: 'creativenatureplayschool@gmail.com',
+        cc: '',//'creativenatureplayschool@gmail.com',
         text: "", // plain text body
         html: await ejs.renderFile('./views/emailTemplate.ejs', {
           name: name,
