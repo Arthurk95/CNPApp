0da7a9b04a9101d7dfddc78c4e1174243b2df877
Matthew Kint <matt.kint@gmail.com>
2020-07-06 13:40:26 -0700

    reset

--- myapp/public/javascripts/emailerScripts.js
+++ myapp/public/javascripts/emailerScripts.js
@@ -502,7 +502,7 @@ function setUnsavedStyle_DEV() {
         document.getElementById('reviewButton').classList.add('disabledButton');
     }
     setSendButtonStatus();
-    document.getElementById('totalEmails').innerHTML = `(${approvedTotal()} Total)`;
+    // document.getElementById('totalEmails').innerHTML = `(${approvedTotal()} Total)`;
 }
 
 function setSavedStyle_DEV() {
@@ -537,7 +537,7 @@ function setSavedStyle_DEV() {
         document.getElementById('reviewButton').classList.remove('disabledButton');
     }
     setSendButtonStatus();
-    document.getElementById('totalEmails').innerHTML = `(${approvedTotal()} Total)`;
+    // document.getElementById('totalEmails').innerHTML = `(${approvedTotal()} Total)`;
 }
 
 function setApprovedStyle_DEV() {
